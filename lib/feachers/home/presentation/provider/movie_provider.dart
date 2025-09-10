42deb07f04756bbe7ad32ff5a27ef8eaa96cd794
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-10 16:48:05 +0330

    filtter movies provider

--- lib/feachers/home/presentation/provider/movie_provider.dart
+++ lib/feachers/home/presentation/provider/movie_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:movie_club/core/constant/constant.dart';
+import 'package:movie_club/feachers/home/data/model/homepage_model.dart';
 import 'package:movie_club/feachers/home/data/model/movies_model.dart';
 import 'package:movie_club/feachers/home/data/repository/movie_repository.dart';
 
@@ -8,12 +10,36 @@ class MovieProvider extends ChangeNotifier {
   MovieProvider(this._movieRepository) {
     fetchMovies();
   }
-
+  final HomePageData _homePageData = HomePageDataConstants.homePageData;
   List<MoviesModel> _moives = [];
   List<MoviesModel> get movies => _moives;
 
   fetchMovies() async {
     _moives = await _movieRepository.fetchMovie();
     notifyListeners();
+  }
+
+  List<MoviesModel> getTrendingMovies() {
+    return _moives
+        .where((movie) => _homePageData.trending.contains(movie.id))
+        .toList();
+  }
+
+  List<MoviesModel> getNewReleaseMovie() {
+    return _moives
+        .where((hotel) => _homePageData.newReleaseMovie.contains(hotel.id))
+        .toList();
+  }
+
+  List<MoviesModel> getnewReleaseTv() {
+    return _moives
+        .where((hotel) => _homePageData.newReleaseTv.contains(hotel.id))
+        .toList();
+  }
+
+  List<MoviesModel> getRecommendedMovies() {
+    return _moives
+        .where((hotel) => _homePageData.recommended.contains(hotel.id))
+        .toList();
   }
 }
