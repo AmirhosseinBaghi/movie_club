12c30c44b278885ebc6904433ca70ebbb1a9d65a
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-10 16:45:15 +0330

    add home page data

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,3 +1,19 @@
+import 'package:movie_club/feachers/home/data/model/homepage_model.dart';
+
 class AppConstant {
   static const String baseUrl = 'https://moviesapi.ir/api/v1';
+}
+
+class HomePageDataConstants {
+  static const List<int> _trendingIds = [1, 3, 5, 7];
+  static const List<int> _newReleaseMovieIds = [2, 4, 6, 8];
+  static const List<int> _newReleaseTvIds = [1, 4, 9];
+  static const List<int> _recommendedIds = [3, 6, 9, 10];
+
+  static HomePageData get homePageData => HomePageData(
+    trending: _trendingIds,
+    newReleaseMovie: _newReleaseMovieIds,
+    newReleaseTv: _newReleaseTvIds,
+    recommended: _recommendedIds,
+  );
 }
---
+++ lib/feachers/home/data/model/homepage_model.dart
@@ -0,0 +1,13 @@
+class HomePageData {
+  final List<int> newReleaseMovie;
+  final List<int> newReleaseTv;
+  final List<int> recommended;
+  final List<int> trending;
+
+  HomePageData({
+    required this.trending,
+    required this.newReleaseMovie,
+    required this.newReleaseTv,
+    required this.recommended,
+  });
+}
