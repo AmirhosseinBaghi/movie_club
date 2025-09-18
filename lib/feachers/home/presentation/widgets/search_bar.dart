95898d714f72a5853a62a1b2fc174e815ed47a9d
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-18 17:23:10 +0330

    use provider to searchBarWidget

--- lib/feachers/home/presentation/widgets/search_bar.dart
+++ lib/feachers/home/presentation/widgets/search_bar.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:movie_club/feachers/home/presentation/provider/movie_provider.dart';
+import 'package:provider/provider.dart';
 
-class SearchBarWidget extends StatelessWidget {
+class SearchBarWidget extends StatefulWidget {
   const SearchBarWidget({super.key});
 
   @override
+  State<SearchBarWidget> createState() => _SearchBarWidgetState();
+}
+
+class _SearchBarWidgetState extends State<SearchBarWidget> {
+  final TextEditingController _controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<MovieProvider>(context);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(elevation: 0),
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsetsGeometry.symmetric(horizontal: 16, vertical: 8),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
 
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration(
                 contentPadding: EdgeInsets.symmetric(horizontal: 12),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(24),
                 ),
-                prefixIcon: Icon(Icons.search_rounded),
+                prefixIcon: IconButton(
+                  icon: Icon(Icons.search_rounded),
+                  onPressed: () {
+                    provider.searchMovie(_controller.text);
+                  },
+                ),
                 hint: Text(
                   'Search movies...',
                   style: TextStyle(color: Colors.grey, fontSize: 14),
@@ -26,6 +41,87 @@ class SearchBarWidget extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(height: 16),
+          provider.isLoading
+              ? Center(child: CircularProgressIndicator())
+              : provider.errorMassage != null
+              ? Column(children: [Center(child: Text(provider.errorMassage!))])
+              : Expanded(
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          childCount: provider.moviesearch.length,
+                          (context, index) {
+                            final movieindex = provider.moviesearch[index];
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 6,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.white),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 80,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(12),
+                                        bottomLeft: Radius.circular(12),
+                                      ),
+
+                                      child: Image.network(movieindex.poster),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 8,
+                                        vertical: 6,
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            movieindex.title,
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          SizedBox(height: 6),
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                Icons.calendar_month_rounded,
+                                              ),
+                                              SizedBox(width: 4),
+                                              Text(movieindex.year),
+                                              SizedBox(width: 20),
+                                              Icon(Icons.star),
+                                              SizedBox(width: 4),
+                                              Text(movieindex.imdbRating),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
         ],
       ),
     );
