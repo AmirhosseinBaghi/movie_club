c2f69d6c0bcc79d6b57d05b37411898ebda10e26
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-18 17:16:14 +0330

    add search api service

--- lib/shared/services/api_service.dart
+++ lib/shared/services/api_service.dart
@@ -13,4 +13,17 @@ class ApiService {
       throw Exception('faild to load moives : $e');
     }
   }
+
+  Future<List<MoviesModel>> searchMovie(String query) async {
+    try {
+      Response response = await dio.get(
+        '/movies',
+        queryParameters: {'q': query},
+      );
+      List<dynamic> moviesList = response.data["data"];
+      return moviesList.map((movie) => MoviesModel.fromJson(movie)).toList();
+    } catch (e) {
+      throw Exception('faild to load moives : $e');
+    }
+  }
 }
