59bfa1fae932d58e502bd9fc7998bd1063246116
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-21 16:33:45 +0330

    fix:api service page number(query)

--- lib/shared/services/api_service.dart
+++ lib/shared/services/api_service.dart
@@ -4,9 +4,12 @@ import 'package:movie_club/feachers/home/data/model/movies_model.dart';
 
 class ApiService {
   final Dio dio = Dio(BaseOptions(baseUrl: AppConstant.baseUrl));
-  Future<List<MoviesModel>> loadMovie() async {
+  Future<List<MoviesModel>> loadMovie(int pageIndex) async {
     try {
-      Response response = await dio.get('/movies');
+      Response response = await dio.get(
+        '/movies',
+        queryParameters: {'page': pageIndex},
+      );
       List<dynamic> moviesList = response.data["data"];
       return moviesList.map((movie) => MoviesModel.fromJson(movie)).toList();
     } catch (e) {
