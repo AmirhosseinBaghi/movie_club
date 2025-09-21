e0710c288cbc51d5d8346c8cd2e6e3b7da7d742f
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-21 16:35:47 +0330

    edit movie card , add size to constractor

--- lib/feachers/home/presentation/widgets/movie_card.dart
+++ lib/feachers/home/presentation/widgets/movie_card.dart
@@ -3,26 +3,34 @@ import 'package:movie_club/feachers/home/data/model/movies_model.dart';
 
 class MovieCard extends StatelessWidget {
   final MoviesModel movies;
-  const MovieCard({super.key, required this.movies});
+  final double width;
+  final double height;
+
+  const MovieCard({
+    super.key,
+    required this.movies,
+    this.width = 120, // عرض پیش‌فرض
+    this.height = 160, // ارتفاع تصویر پیش‌فرض
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 8, left: 12, right: 12),
+      padding: const EdgeInsets.all(8.0),
       child: SizedBox(
-        width: 120,
+        width: width,
         child: Column(
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(8),
               child: Image.network(
                 movies.poster,
-                width: 120,
-                height: 160,
+                width: width,
+                height: height,
                 fit: BoxFit.cover,
               ),
             ),
-            SizedBox(height: 4),
+            const SizedBox(height: 4),
             Text(
               movies.title,
               maxLines: 2,
