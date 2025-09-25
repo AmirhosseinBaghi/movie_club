af42aeffa8a0dac38b03da7d0ec000cc39822211
amirhossein baghi <dev.amir.b22@gmail.com>
2025-09-25 15:13:17 +0330

    add detailScreen

--- lib/feachers/moive_detail/detail_screen.dart
+++ lib/feachers/moive_detail/detail_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:movie_club/feachers/moive_detail/data/model/movie_detail_model.dart';
+import 'package:movie_club/feachers/moive_detail/data/repository/detail_repository.dart';
 import 'package:movie_club/feachers/moive_detail/presentation/provider/detail_provier.dart';
-
-import 'package:provider/provider.dart';
+import 'package:movie_club/shared/services/api_service.dart';
 
 class DetailScreen extends StatelessWidget {
   final String id;
@@ -9,38 +10,148 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future.microtask(() {
-      context.read<DetailProvider>().fetchMovie(id);
-    });
-
-    return Scaffold(
-      appBar: AppBar(title: const Text("Movie Detail")),
-      body: Consumer<DetailProvider>(
-        builder: (context, provider, child) {
-          if (provider.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (provider.movieDetail == null) {
-            return const Center(child: Text("No data"));
-          }
-
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Title: ${provider.movieDetail!.title}",
-                  style: const TextStyle(fontSize: 20),
+    return FutureBuilder<MoviedetailModel>(
+      future: DetailProvider(
+        DetailScreenRepository(apiService: ApiService()),
+      ).fetchMovie(id),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        final movie = snapshot.data!;
+        return Scaffold(
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                flexibleSpace: FlexibleSpaceBar(
+                  background: GestureDetector(
+                    onLongPress: () {},
+                    child: Image.network(movie.poster, fit: BoxFit.fill),
+                  ),
                 ),
-                const SizedBox(height: 10),
-                Text("Actors: ${provider.movieDetail!.actors}"),
-              ],
-            ),
-          );
-        },
-      ),
+                expandedHeight: 300,
+                elevation: 8,
+                leading: BackButton(color: Colors.white),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 16,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      SizedBox(height: 12),
+                      Wrap(
+                        spacing: 12,
+                        children: movie.genres.map((e) {
+                          return Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 8,
+                              vertical: 6,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(24),
+                              color: Colors.white,
+                            ),
+                            child: Text(
+                              e,
+                              style: Theme.of(context).textTheme.bodyMedium!
+                                  .copyWith(color: Color(0xFF34344A)),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.timer_sharp),
+                              SizedBox(width: 4),
+                              Text(
+                                movie.runtime,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.calendar_month_outlined),
+                              SizedBox(width: 4),
+                              Text(
+                                movie.year,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.star),
+                              SizedBox(width: 4),
+                              Text(
+                                movie.imdbRating,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                          Text(
+                            '${movie.imdbVotes}k Review',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 24),
+                      Text(
+                        movie.plot,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      SizedBox(height: 24),
+                      Text(
+                        'Details',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Text(
+                            'Country : ',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Text(
+                            movie.country,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Text(
+                            'Date Release : ',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          Text(
+                            movie.released,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
